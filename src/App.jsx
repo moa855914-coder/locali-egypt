fdf65ecca1356df1fa71d0e2434ef4d00de5376d
Mo Adel <moa855914@gmail.com>
2026-04-09 16:09:22 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import WomenSafety from './pages/WomenSafety';
 import Deals from './pages/Deals';
 import CityPrices from './pages/city/CityPrices';
 import EgyptSafeNow from './pages/EgyptSafeNow';
-import EgyptVsDubai from './pages/EgyptVsDubai';
+import EgyptVsDubai from './pages/EgyptVsDubai.jsx';
 import LastMinuteEgypt from './pages/LastMinuteEgypt';
 import Nightlife from './pages/Nightlife';
 import RemoteWork from './pages/RemoteWork';
